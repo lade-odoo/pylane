260cf6391dcfbab4e5891a18984c8a100f4adc95
Laurent Desausoi <lade@odoo.com>
2024-06-03 15:29:17 +0200

    fix: ipython version

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 
+import sys
+
 from setuptools import setup
 from os import path
 
 
-VERSION = '0.0.11'
+VERSION = '0.0.12'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
+# Dirty hack to differentiate hosts in 22.04 & 24.04
+ipython_max_version = '8.0' if sys.version_info < (3, 12) else '8.20'
 
 setup(
     name='pylane',
@@ -35,7 +39,7 @@ setup(
     # },
     install_requires=[
         'ipython==5.8;python_version<"3.4"',
-        'ipython>=7.2,<8.0;python_version>="3.4"',
+        f'ipython>=7.2,<={ipython_max_version};python_version>="3.4"',
         "Click==7.0",
     ],
     keywords=['debug', 'attach', 'gdb', 'shell']
